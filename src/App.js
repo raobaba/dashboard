87f5bdfd93de667eb6951a6ad621ff821ee248a1
Rajan Kumar <raorajan9576@gmail.com>
2024-08-30 01:22:04 +0530

    sidebar created

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import {
 import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
@@ -41,8 +42,22 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
-
-          <div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
