8efb138449c8ab18a83d17f7354dbe24dc25fc34
Rajan Kumar <raorajan9576@gmail.com>
2024-08-31 21:28:50 +0530

    navbar created

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,14 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } =
+    useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
@@ -22,6 +29,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -30,6 +38,7 @@ const Sidebar = () => {
               <button
                 type="button"
                 onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4"
               >
                 <MdOutlineCancel />
@@ -46,8 +55,9 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
+                    onClick={handleCloseSideBar}
                     style={({ isActive }) => ({
-                      backgroundColor: isActive ? "black" : "",
+                      backgroundColor: isActive ? currentColor : "",
                     })}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,12 +11,22 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
 
   return (
     <StateContext.Provider
       value={{
         activeMenu,
         setActiveMenu,
+        screenSize,
+        setScreenSize,
+        isClicked,
+        setIsClicked,
+        handleClick,
       }}
     >
       {children}
